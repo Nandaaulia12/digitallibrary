babd91207fd216d2ba60f9b084d46074c65cb29c
Nandaaulia12 <nandaaulianurfauzan@gmail.com>
2024-02-29 13:32:30 +0700

    1

---
+++ layouts/dashboard/footer.php
@@ -0,0 +1,45 @@
+<!-- Main Footer -->
+</section>
+</div>
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024-2025 <a href="https://adminlte.io">madebynaje</a>.</strong>
+  
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 2.7.3
+    </div>
+  </footer>
+</div>
+<!-- Bootstrap -->
+<script src="adminv/dist/js/jquery.js"></script>
+<script src="adminv/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="adminv/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="adminv/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="adminv/plugins/jszip/jszip.min.js"></script>
+<script src="adminv/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="adminv/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="adminv/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="adminv/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="adminv/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+
+
+
+<!-- AdminLTE -->
+<script src="adminv/dist/js/adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="adminv/plugins/chart.js/Chart.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="adminv/dist/js/demo.js"></script>
+
+<script src="adminv/dist/js/datatable.js"></script>
+<script>
+    let table = new DataTable('#myTable', {
+    // config options...
+});
+</script>
+<script src="adminv/dist/js/pages/dashboard3.js"></script>
+
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+</body>
+</html>
